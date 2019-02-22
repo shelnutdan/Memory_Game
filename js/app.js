c79a6a8750d58fd0d4bf496977f5932bd729d5a0
shelnutdan <shelnutdaniel@gmail.com>
2019-02-21 21:48:05 -0800

    Fourth commit: resolve issues with the game grid to prevent double clicks. incorporate code that I used to for my final project on codecademy for a mathcing gamr. Added the timing function, start button figures. still have the reset button to program in and the rating system

--- js/app.js
+++ js/app.js
@@ -1,17 +1,233 @@
+let MatchGame={};
+let count=0;
+let matchCount=0;
+
 $(document).ready(function(){
-  /*Constructing randomized grid and placing it on the DOM*/
+  let $gameGrid = $('.grid-container');
+
+  $('.start').on('click',function(){
+    timer();
+    //let time =setInterval(myTimer,100)
+    let cardNumbers=MatchGame.createCardNumbers()
+
+    MatchGame.createCards(cardNumbers,$gameGrid);
+    console.log(matchCount)
+
+  })
+})
+  //console.log(Object.keys(MatchGame).length)
+  /*Generates array of match card numbers that will be uiused later for comparsions*/
+  //let cardNumbers=MatchGame.createCardNumbers()
+  //console.log(cardNumbers)
+  //MatchGame.createCards(cardNumbers,$gameGrid);
+  /*$(function(){
+
+/*
+time function taken from W3school link: https://www.w3schools.com/jsref/met_win_setinterval.asp*/
+/*
+function myTimer() {
+
+  let startTime = new Date();
+  let timeString = startTime.toLocaleTimeString();
+  document.getElementById('timeDisplay').innerHTML = timeString;
+}
+/**/
+/* Utilized code from daniel hug link:https://jsfiddle.net/Daniel_Hug/pvk6p/*/
+let seconds = 0;
+let minutes = 0;
+let time;
+
+function add() {
+seconds++;
+if (seconds >= 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes >= 60) {
+        minutes = 0;
+
+    }
+}
+
+document.getElementById('timeDisplay').innerHTML=  (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+timer();
+}
+function timer() {
+time = setTimeout(add, 1000);
+}
+
+
+
+
+/* Start button
+
+
+
+resetButton
+rating system
+
+function rating(time, moves){
+  if (count<26 & time)
+}
+*/
+/*
+    $('.btn-primary').on('click', function(){
+      let $gameGrid = $('.grid-container');
+
+      $('.container').css({'display':'block'})
+      let cardNumbers=MatchGame.createCardNumbers()
+
+      MatchGame.createCards(cardNumbers,$gameGrid);
+      console.log(matchCount)
+
+    });
+});*/
+
+/*Generates array of match card numbers that will be uiused later for comparsions*/
+function endGame(){
+  matchCount=0;
+
+  $(document).ready(function(){
+    $("#myModal").modal('show');
+
+    $('.container').css({'display':"none"})
+    $('.btn-primary').on('click', function(){
+      let $gameGrid = $('.grid-container');
+
+      $('.container').css({'display':'block'})
+      let cardNumbers=MatchGame.createCardNumbers()
+
+      MatchGame.createCards(cardNumbers,$gameGrid);
+      console.log(matchCount)
+
+    });
+  });
+}
+
+
+MatchGame.createCardNumbers=function(){
+  let cardArray=[];
+  for (let i=1;i<=8;i++){
+    cardArray.push(i);
+    cardArray.push(i);
+  }
+
+  let numberArray=[];
+  while (cardArray.length != 0){
+    let randomNum=Math.floor(Math.random() * cardArray.length);
+    let cardValue=cardArray.splice(randomNum,1)[0]
+    numberArray.push(cardValue);
+
+  }
+  return numberArray;
+}
+
+/*Takes the card numbers and converts into game objects*/
+MatchGame.createCards= function(cardNumbers,$gameGrid){
+/*have list of colors for the cards to take when flipped over*/
+  let colors = [
+    'hsl(25, 85%, 65%)',
+    'hsl(55, 85%, 65%)',
+    'hsl(90, 85%, 65%)',
+    'hsl(160, 85%, 65%)',
+    'hsl(220, 85%, 65%)',
+    'hsl(265, 85%, 65%)',
+    'hsl(310, 85%, 65%)',
+    'hsl(360, 85%, 65%)']
+  /*Empty out the game grid to */
+  $gameGrid.empty()
+  $gameGrid.data('flippedCards',[]);
+  for (let i=0;i<cardNumbers.length;i++){
+    let cardNum=cardNumbers[i];
+    let color=colors[cardNum-1];
+    let data={
+      cardNum: cardNum,
+      color:color,
+      flipped: false
+    };
+    let $cardElement=$("<div class='card'></div>");
+    $cardElement.data(data);
+    $gameGrid.append($cardElement);
+  }
+  $('.card').click(function(){
+    MatchGame.cardFlip($(this),$gameGrid);
+  })
+};
+
+
+
+
+
+MatchGame.cardFlip= function($card,$game){
+  /*Prevents double clicks on the same card*/
+  let timeStart= Date.now()
+  if ($card.data('flipped')){
+    return
+  }
+
+  $card.css('background-color', $card.data('color'))
+      .text($card.data('cardNum'))
+      .data('flipped', true);
+  let cardsFlip=$game.data('flippedCards')
+  cardsFlip.push($card);
+
+
+
+  if (cardsFlip.length === 2) {
+    if (cardsFlip[0].data('cardNum') === cardsFlip[1].data('cardNum')) {
+      let matched = {
+        backgroundColor: 'rgb(153, 153, 153)',
+        color: 'rgb(204, 204, 204)'
+      };
+      cardsFlip[0].css(matched);
+      cardsFlip[1].css(matched);
+      count+=2;
+      matchCount+=2;
+      //console.log(cardsFlip[0])
+      //console.log(cardsFlip[1])
+      console.log(count);
+      console.log(matchCount)
+    } else {
+      let firstCard = cardsFlip[0];
+      var secondCard= cardsFlip[1];
+      count+=2;
+      console.log(count);
+      window.setTimeout(function() {
+        firstCard.css('background-color', 'white')
+            .text('')
+            .data('flipped', false);
+        secondCard.css('background-color', 'white')
+            .text('')
+            .data('flipped', false);
+      }, 350);
+    }
+    $game.data('flippedCards', []);
+    if (matchCount==2){
+      clearTimeout(t);/*
+      let timeEnd= Date.now()
+
+      console.log(timeStart);
+      console.log(timeEnd)
+      console.log(timeEnd-timeStart)*/
+      endGame();
+    }
+  }
+}
+
+
+  /*Constructing randomized grid and placing it on the DOM*
   console.log('javascript is running?');
   let appendArray = $('.grid-container');
   let cardArray=appendArray.children();
   console.log(cardArray);
 
-  /*Start game function*/
+  /*Start game function*
   function shuffleGrid() {
 
     console.log(cardArray);
-        /*Shuffling function for the array of the cardArray*/
-        /*shuffle function taken from https://jsfiddle.net/C6LPY/2*/
-    while (cardArray.length>0) {
+        /*Shuffling function for the array of the cardArray*
+        /*shuffle function taken from https://jsfiddle.net/C6LPY/2*
+    while (cardArray.length!=0) {
           appendArray.append(cardArray.splice(Math.floor(Math.random() * cardArray.length)));
       }
 
@@ -25,31 +241,11 @@ $(document).ready(function(){
   })
   $('.reset').on('click',function(){
     console.log("Resetting the game");
+    cardArray.empty();
     shuffleGrid()
     startGame();
   })
-  //let matchCards=[];
-  /*function startGame(){
-    while(matchCards.length<16){
-
-    } console.log('game is over!')
-
-  }
-
-  */
-
-  /*shuffle grid function*/
-  /*Turn function*/
-  /*
-  function turnFunction(){
-    while (matchCards.length!=16){
-      let round=0;
-      cardClicks();
-
-  }
-
-}
-Start game function*/
+/*Start game function*
 function startGame(){
   shuffleGrid();
 
@@ -62,17 +258,13 @@ function startGame(){
 }
 /*resetButton
 
-/* endGame function*/
+/* endGame function*
 function endGame(){
-let timeEnd=Date.now;
-let total= timeEnd=timeStart;
-console.log(timeStart);
-console.log(timeEnd);
-console.log(total)
-  alert('Congrats you have won!');
+
+  setTimeout(alert('Congrats you have won!'),30000);
 }
 
-  /*comparsions function*/
+  /*comparsions function*
 function matchCards(){
    firstCard.addClass('card-match')
    secondCard.addClass('card-match')
@@ -81,7 +273,7 @@ function matchCards(){
    matchCard.push(firstCard,secondCard)
    console.log(matchCard.length);
 }
-/*reset card function*/
+/*reset card function*
 function resetCards(){
   firstCard.removeClass('card-flip');
   secondCard.removeClass('card-flip');
@@ -89,27 +281,8 @@ function resetCards(){
   secondCard='';
   count=0;
 }
-/*  obsoluete function
 
-function compareCard(){
-    if (tempCards[0].isEqualNode(tempCards[1])){
-      matchCards.push(tempCards[0]);
-      matchCards.push(tempCards[1]);
-      tempCards[0].classList.add('card-match');
-      tempCards[1].classList.add('card-match');
-      console.log(matchCards.length);
-      //delete tempCards[0];
-      //delete tempCards[1];
-      console.log(tempCards);
-  }else{
-    tempCards[0].classList.remove('card-flip')
-    tempCards[1].classList.remove('card-flip');
-    //delete tempCards[0];
-    //delete tempCards[1];
-  }
-}
 
-*/
 
 
   /*reset function*/
@@ -118,57 +291,36 @@ function compareCard(){
   /*document.getElementsByClassName('.card').addEventListener('click', function(){
       document.getElementsByClassName('.card').css({"background-color":"green"});
     },false
-  );*/
+
   /*temporary stoarge for cards in a turn*/
-  //let tempCards=[];
+
 
   /*Allows for the cards to be flipped
   and then stored into a temporary variable for later comparsions*/
-/*  $('.grid-container').on('click',"div",function(){
-    $(this).addClass('card-flip');
-    tempCards.push(this);
-    //console.log(tempCards);
-  })*/
+
   //console.log(cards);
   /*Logic for a turn: a turn is occuring while the array tempCards is less than length 2*/
   /*Use a while loop that */
 
- /* Card flipping functionality*/
-/*let tempCards=[];
-let count=0;
-let turnCounter=0;
-let firstCard="";
-let secondCard="";
-let currentSelection=null;
-let matchCard=[];*/
+ /* Card flipping functionality*
+
 let count=0;
 let turnCounter=0;
 let firstCard="";
 let secondCard="";
 let matchCard=[];
 function cardClicks(){
-  
 
-
-  let currentSelection=null;
 
   $('.grid-container').on('click',"div",function(){
 
-    //$(this).addClass('card-flip');
-    //firstCard=$(this).text();
-    //console.log(firstCard);
-    //let secondcard="";
-    //console.log(count);
-    /*if ($(this)==currentSelection){
-      return;
-    }*/
+
 
     if (count<2){
       count+=1;
       $(this).addClass('card-flip')
 
       if (count ==1){
-        //firstCard=$(this).dataset.id;
 
         firstCard=$(this)
         currentSelection=firstCard;
@@ -177,9 +329,7 @@ function cardClicks(){
         secondCard=$(this)
       }
        if (!$(this).hasClass('card-flip')){
-        //secondCard=$(this).dataset.id;
-        //$(this).addClass('card-flip')
-        //secondCard=$(this)
+
         return ;
       }
 
@@ -201,31 +351,13 @@ function cardClicks(){
         }
       }
       //console.log(tempCards)
-
+      if (matchCard.length===16){
+        endGame();
+      }
     }
     //console.log(tempCards);
     /*Place logic for determine if cards match */
-  })
+/*  })
+
 }
-
-
- /*
-    $('.grid-container').on('click',"div",function(){
-
-      $(this).addClass('card-flip');
-      tempCards.push(this);
-      count+=1;
-      //console.log(count);
-      if (count===2){
-        compareCard()
-
-      }
-    })
-    */
-      //console.log(tempCards);
-      /*Place logic for determine if cards match */
-
-    //cardClicks();
-
-    //console.log(tempCards);
-  });
+})*/
